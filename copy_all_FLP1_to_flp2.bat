c9eb8ed1d7a1ac5ba98ad472ac19dd6edde1be6f
Martin pávek <martin_pavek@email.cz>
2025-02-18 12:23:42 +0100

    Add files via upload

---
+++ copy_all_FLP1_to_flp2.bat
@@ -0,0 +1,8 @@
+@echo off
+del /F /Q C:\Users\DELL\Documents\QL_SAVE\FLP2\*.*
+sleep 1
+copy /V C:\Users\DELL\Documents\QL_SAVE\FLP1\*.* C:\Users\DELL\Documents\QL_SAVE\FLP2\
+REM sleep 1
+@echo on
+
+
